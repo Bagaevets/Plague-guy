a0e9f449e61ff350b713b561e2de54e9703c13d2
Bagayevets <ka4ergd@gmail.com>
2025-09-18 23:52:51 +0300

    Добавлен переход на сцену осмотра пациента

--- Assets/Plague guy/Scripts/Interactions/InteractablePatient.cs
+++ Assets/Plague guy/Scripts/Interactions/InteractablePatient.cs
@@ -1,11 +1,15 @@
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InteractablePatient : MonoBehaviour, IInteractable
 {
     [Header("Patient settings")]
     [SerializeField] private string _interactionPrompt = "Осмотреть пациента";
     [SerializeField] private bool _canInteract = true;
+
+    [Header("Scene Settings")]
+    [SerializeField] private string _inspectionSceneName = "InspectionScene";
 
     public string InteractionPrompt => _interactionPrompt;
     public bool CanInteract => _canInteract;
@@ -20,8 +24,20 @@ public class InteractablePatient : MonoBehaviour, IInteractable
         Debug.Log("Метод Interact() вызван!");
         Debug.Log("Начинаем осморт пациента");
         //...
+        LoadInspectionScene();
     }
-
+    private void LoadInspectionScene()
+    {
+        if (!string.IsNullOrEmpty(_inspectionSceneName))
+        {
+            Debug.Log($"Загружаем сцену осмотра: {_inspectionSceneName}");
+            SceneManager.LoadScene(_inspectionSceneName);
+        }
+        else
+        {
+            Debug.LogError("Название сцены осмотра не указано!");
+        }
+    }
     public void SetInteractable(bool canInteract)
     { 
      _canInteract = canInteract;  
